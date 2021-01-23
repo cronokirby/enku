5acccd6e877d165ba099757be026a4c41326aa61
Lúcás Meier <cronokirby@gmail.com>
2021-01-23 21:07:10 +0100

    Use correct key size

--- src/Key.cpp
+++ src/Key.cpp
@@ -1,9 +1,10 @@
 #include "Key.hpp"
 #include "random.hpp"
 #include <assert.h>
+#include <iostream>
 
-// We use 256 bits, since that's the size mandated by ChaCha20
-constexpr uint32_t KEY_SIZE = 256;
+// We use 32 * 8 = 256 bits, since that's the size mandated by ChaCha20
+constexpr uint32_t KEY_SIZE = 32;
 
 Key::Key() {
   data = new uint8_t[KEY_SIZE];
@@ -34,26 +35,28 @@ constexpr char base64(uint8_t c) {
   return BASE64_TABLE[c];
 }
 
-constexpr char *PEM_START = "-----BEGIN ENKU PRIVATE KEY-----\n";
-constexpr char *PEM_END = "-----END ENKU PRIVATE KEY-----\n";
+constexpr char PEM_START[] = "-----BEGIN ENKU PRIVATE KEY-----\n";
+constexpr char PEM_END[] = "\n-----END ENKU PRIVATE KEY-----\n";
 
 void Key::write_pem(std::ostream &stream) {
   stream << PEM_START;
-  uint32_t block_size = 1 << 5;
-  uint32_t blocks = KEY_SIZE >> 5;
-  for (uint32_t block = 0; block < blocks; ++block) {
-    for (uint32_t bi = 0; bi < block_size; bi += 3) {
-      uint32_t j = (block << 5) | bi;
-      uint8_t x1 = data[j];
-      uint8_t x2 = data[j + 1];
-      uint8_t x3 = data[j + 2];
 
-      stream.put(base64(x1 & 0x3F));
-      stream.put(base64((x2 & 0xF) | (x1 >> 6)));
-      stream.put(base64((x3 & 0x3) | (x2 >> 4)));
-      stream.put(base64(x3 >> 6));
-    }
-    stream.put('\n');
+  for (uint32_t i = 0; i + 2 < KEY_SIZE; i += 3) {
+    std::cout << i << '\n';
+    uint8_t x1 = data[i];
+    uint8_t x2 = data[i + 1];
+    uint8_t x3 = data[i + 2];
+
+    stream.put(base64(x1 & 0x3F));
+    stream.put(base64((x2 & 0xF) | (x1 >> 6)));
+    stream.put(base64((x3 & 0x3) | (x2 >> 4)));
+    stream.put(base64(x3 >> 6));
   }
+  uint8_t x1 = data[KEY_SIZE - 2];
+  uint8_t x2 = data[KEY_SIZE - 1];
+  stream.put(base64(x1 & 0x3F));
+  stream.put(base64((x2 & 0xF) | (x1 >> 6)));
+  stream.put(base64(x2 >> 4));
+
   stream << PEM_END;
 }
